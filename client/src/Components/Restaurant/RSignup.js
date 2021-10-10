3b238778325b18c7dd3437f4144677651803b4c7
Dhruvi <dhruvi.m.shah@gmail.com>
2021-10-09 20:24:36 -0700

    Installed Packages and Validations

--- client/src/Components/Restaurant/RSignup.js
+++ client/src/Components/Restaurant/RSignup.js
@@ -1,48 +1,73 @@
 import React, { useState } from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+} from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbarls from "../Navbar/Navbarls";
 import { Axios } from "../../axios";
 import { Config } from "../../config";
 
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+import Input from "@mui/material/Input";
+
 const theme = createTheme();
 
 export default function RSignUp() {
-  const [nameReg, setNameReg] = useState("");
-  const [emailReg, setEmailReg] = useState("");
-  const [contactReg, setContactReg] = useState("");
-  const [descriptionReg, setDescriptionReg] = useState("");
-  const [locationReg, setLocationReg] = useState("");
-  const [typeReg, setTypeReg] = useState("");
-  const [timeReg, setTimeReg] = useState("");
-  const [deliveryReg, setDeliveryReg] = useState("");
-  const [dayReg, setDayReg] = useState("");
-  const [passwordReg, setPasswordReg] = useState("");
-  const rsignup = () => {
+  // const [nameReg, setNameReg] = useState("");
+  // const [emailReg, setEmailReg] = useState("");
+  // const [contactReg, setContactReg] = useState("");
+  // const [descriptionReg, setDescriptionReg] = useState("");
+  // const [locationReg, setLocationReg] = useState("");
+  // const [typeReg, setTypeReg] = useState("");
+  // const [timeReg, setTimeReg] = useState("");
+  // const [deliveryReg, setDeliveryReg] = useState("");
+  // const [dayReg, setDayReg] = useState("");
+  // const [passwordReg, setPasswordReg] = useState("");
+  // const rsignup = () => {
+  //   Axios.post(Config.url + "/restaurant", {
+  //     Restaurant_Name: nameReg,
+  //     Restaurant_Email: emailReg,
+  //     Restaurant_Description: descriptionReg,
+  //     Restaurant_Contact: contactReg,
+  //     Restaurant_Type: typeReg,
+  //     Restaurant_Time: timeReg,
+  //     Restaurant_Delivery: deliveryReg,
+  //     Restaurant_Day: dayReg,
+  //     Restaurant_Location: locationReg,
+  //     Restaurant_Password: passwordReg,
+  //   }).then((response) => {
+  //     console.log(response);
+  //   });
+  // };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const { name, email, password } = data;
     Axios.post(Config.url + "/restaurant", {
-      Restaurant_Name: nameReg,
-      Restaurant_Email: emailReg,
-      Restaurant_Description: descriptionReg,
-      Restaurant_Contact: contactReg,
-      Restaurant_Type: typeReg,
-      Restaurant_Time: timeReg,
-      Restaurant_Delivery: deliveryReg,
-      Restaurant_Day: dayReg,
-      Restaurant_Location: locationReg,
-      Restaurant_Password: passwordReg,
+      Restaurant_Name: name,
+      Restaurant_Email: email,
+      Restaurant_Password: password,
     }).then((response) => {
       console.log(response);
     });
   };
-
+  //console.log(errors);
   return (
     <ThemeProvider theme={theme}>
       <div>
@@ -67,151 +92,122 @@ export default function RSignUp() {
 
           <Box
             component="form"
-            // onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             sx={{ mt: 3 }}
+            noValidate
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="rname"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Restaurant Name"
-                  autoFocus
-                  onChange={(e) => {
-                    setNameReg(e.target.value);
-                  }}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  onChange={(e) => {
-                    setEmailReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="description"
-                  label="Description"
-                  name="description"
-                  autoComplete="description"
-                  onChange={(e) => {
-                    setDescriptionReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="contact"
-                  label="Mobile Number"
-                  name="contact"
-                  autoComplete="contact"
-                  onChange={(e) => {
-                    setContactReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="type"
-                  label="Veg, Non-Veg or Vegan"
-                  name="type"
-                  autoComplete="type"
-                  onChange={(e) => {
-                    setTypeReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="time"
-                  label="Time ( HH:MM:SS )"
-                  name="time"
-                  autoComplete="time"
-                  onChange={(e) => {
-                    setTimeReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="delivery"
-                  label="Delivery-Mode"
-                  name="delivery"
-                  autoComplete="delivery"
-                  onChange={(e) => {
-                    setDeliveryReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="day"
-                  label="Days ( From - To )"
-                  name="day"
-                  autoComplete="day"
-                  onChange={(e) => {
-                    setDayReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="location"
-                  label="Location"
-                  name="location"
-                  autoComplete="location"
-                  onChange={(e) => {
-                    setLocationReg(e.target.value);
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={(e) => {
-                    setPasswordReg(e.target.value);
-                  }}
-                />
-              </Grid>
-            </Grid>
+            <FormControl
+              error={
+                errors.name?.type === "required"
+                  ? true
+                  : errors.name?.type === "minLength"
+                  ? true
+                  : errors.name?.type === "maxLength"
+                  ? true
+                  : false
+              }
+              variant="standard"
+              fullWidth
+            >
+              <InputLabel htmlFor="component-name">Restaurant Name</InputLabel>
+              <Input
+                id="component-error-name"
+                aria-describedby="component-error-text"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                  minLength: 2,
+                  maxLength: 30,
+                })}
+              />
+              <FormHelperText id="component-error-text">
+                {errors.name?.type === "required"
+                  ? "Restaurant Name is Required."
+                  : errors.name?.type === "minLength"
+                  ? "Restaurant Name is too short."
+                  : errors.name?.type === "maxLength"
+                  ? "Restaurant Name is too long."
+                  : ""}
+              </FormHelperText>
+            </FormControl>
+            <FormControl
+              error={
+                errors.email?.type === "required"
+                  ? true
+                  : errors.email?.type === "minLength"
+                  ? true
+                  : errors.email?.type === "pattern"
+                  ? true
+                  : false
+              }
+              variant="standard"
+              fullWidth
+            >
+              <InputLabel htmlFor="component-email">
+                Restaurant Email
+              </InputLabel>
+              <Input
+                id="component-error-email"
+                aria-describedby="component-error-text"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                  min: 3,
+                  pattern: /^\S+@\S+$/i,
+                })}
+              />
+              <FormHelperText id="component-error-text">
+                {errors.email?.type === "required"
+                  ? "Restaurant Email is Required."
+                  : errors.email?.type === "minLength"
+                  ? "Restaurant Email is too short."
+                  : errors.email?.type === "pattern"
+                  ? "This is an email."
+                  : ""}
+              </FormHelperText>
+            </FormControl>
+            <FormControl
+              error={
+                errors.password?.type === "required"
+                  ? true
+                  : errors.password?.type === "maxLength"
+                  ? true
+                  : errors.password?.type === "pattern"
+                  ? true
+                  : false
+              }
+              variant="standard"
+              fullWidth
+            >
+              <InputLabel htmlFor="component-password">Password</InputLabel>
+              <Input
+                id="component-error-password"
+                aria-describedby="component-error-text"
+                placeholder="Password"
+                type="password"
+                {...register("password", {
+                  required: true,
+                  maxLength: 80,
+                  pattern:
+                    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/i,
+                })}
+              />
+              <FormHelperText id="component-error-text">
+                {errors.password?.type === "required"
+                  ? "Password is Required."
+                  : errors.password?.type === "maxLength"
+                  ? "You can have only 80 characters."
+                  : errors.password?.type === "pattern"
+                  ? "Minimum eight characters, at least one letter, one number and one special character."
+                  : ""}
+              </FormHelperText>
+            </FormControl>
             <Button
               style={{ background: "#b26a00" }}
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              onClick={rsignup}
             >
               Sign Up
             </Button>
